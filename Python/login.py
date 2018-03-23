1d1df9e0f719c8e92884339ff6c64c1a4e6202b2
shine <shiyaleinl@gmail.com>
2018-03-23 10:36:09 +0800

    move python file

--- Python/login.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#! -*- coding:utf-8 -*-
-def account_login():
-	password=str(input('Please input your password:'))
-	if password == '12345':
-		print('Login Success!')
-	else:
-		print('Password Error!\nPlease try again!')
-account_login()
